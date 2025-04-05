3ba1eb6a3850f4fc0288d923b76115180c533ef8
sky030b <sky030blue030@gmail.com>
2025-04-06 03:12:01 +0800

    fix: modify the params from voteAddress to address

--- frontend/app/polls/[address]/page.tsx
+++ frontend/app/polls/[address]/page.tsx
@@ -30,8 +30,8 @@ interface Poll {
   country: string
 }
 
-export default function PollPage({ params }: { params: { voteAddress: string } }) {
-  const voteAddress = `0x${params.voteAddress}`
+export default function PollPage({ params }: { params: { address: string } }) {
+  const voteAddress = `0x${params.address}`
   const { address, isConnected } = useWallet()
   const [poll, setPoll] = useState<Poll | null>(null)
   const [proof, setProof] = useState(null)
